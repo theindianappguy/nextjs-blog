e2c371e2538243627dd79526b1a85b785cca2611
theindianappguy <theindianappguy@gmail.com>
2021-03-22 13:33:19 +0530

    added routing to each blog post

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import { InferGetStaticPropsType } from "next"
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import styled from "@emotion/styled"
+import Link from 'next/link';
 
 const title: string = "AwesomeBlog";
 
@@ -10,7 +11,7 @@ const Container = styled.div` display:flex; justify-content: center
 const Main = styled.main`max-width: 800px`;
 const BlogTitle = styled.h1`margin:32px 0px`;
 const List = styled.div``;
-const ListItem = styled.div``;
+const ListItem = styled.div`cursor: pointer;`;
 const PostTitle = styled.h2``;
 const PostBody = styled.p``;
 
@@ -28,14 +29,17 @@ export default function Home({ posts }: InferGetStaticPropsType<typeof getStatic
         </BlogTitle>
         <List>
           {posts.map((post) => {
-            return <ListItem key={post.id}>
-              <PostTitle>{post.title}</PostTitle>
-              <PostBody>{post.body}</PostBody>
-            </ListItem>
+
+            return <Link href="/[id]" as={`/${post.id}`}>
+              <ListItem key={post.id}>
+                <PostTitle>{post.title}</PostTitle>
+                <PostBody>{post.body}</PostBody>
+              </ListItem>
+            </Link>
           })}
         </List>
       </Main>
-    </Container>
+    </Container >
   )
 }
 
